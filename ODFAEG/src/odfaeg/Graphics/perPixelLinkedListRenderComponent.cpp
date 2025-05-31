74f7ca2e586160004a0d6b220ee54b77bded9569
LaurentDuroisin7601 <laurentduroisin7601@gmail.com>
2025-06-01 00:29:22 +0200

    bone animations

--- ODFAEG/src/odfaeg/Graphics/perPixelLinkedListRenderComponent.cpp
+++ ODFAEG/src/odfaeg/Graphics/perPixelLinkedListRenderComponent.cpp
@@ -2761,7 +2761,7 @@ namespace odfaeg {
                                                                     totalPosition = vec4(1.f, 1.f, 1.f, 1.f);
                                                                 }
                                                                 uint textureIndex =  materialData.textureIndex;
-                                                                gl_Position = projectionMatrix * viewMatrix * modelData.modelMatrix * vec4(position, 1.f) * vec4((totalPosition.x - xOff), (totalPosition.y + yOff), totalPosition.z, totalPosition.w);
+                                                                gl_Position = projectionMatrix * viewMatrix * modelData.modelMatrix * vec4((position.x - xOff), (position.y + yOff), position.z, 1.f) * totalPosition;
                                                                 fTexCoords = (textureIndex != 0) ? (textureMatrix[textureIndex-1] * vec4(texCoords, 1.f, 1.f)).xy : texCoords;
                                                                 frontColor = color;
                                                                 texIndex = textureIndex;
