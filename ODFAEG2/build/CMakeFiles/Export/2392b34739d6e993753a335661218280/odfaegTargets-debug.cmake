334b6de4a4bc1e3157b021716e7c69879640d7b7
Laurent <laurentduroisin@gmail.com>
2026-07-01 08:59:39 +0200

    bug correction up vector point light view matrix

---
+++ ODFAEG2/build/CMakeFiles/Export/2392b34739d6e993753a335661218280/odfaegTargets-debug.cmake
@@ -0,0 +1,143 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "odfaeg::odfaeg-core" for configuration "Debug"
+set_property(TARGET odfaeg::odfaeg-core APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(odfaeg::odfaeg-core PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libodfaeg-core.a"
+  )
+
+list(APPEND _cmake_import_check_targets odfaeg::odfaeg-core )
+list(APPEND _cmake_import_check_files_for_odfaeg::odfaeg-core "${_IMPORT_PREFIX}/lib/libodfaeg-core.a" )
+
+# Import target "odfaeg::odfaeg-core-mod" for configuration "Debug"
+set_property(TARGET odfaeg::odfaeg-core-mod APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(odfaeg::odfaeg-core-mod PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libodfaeg-core-mod.a"
+  )
+
+list(APPEND _cmake_import_check_targets odfaeg::odfaeg-core-mod )
+list(APPEND _cmake_import_check_files_for_odfaeg::odfaeg-core-mod "${_IMPORT_PREFIX}/lib/libodfaeg-core-mod.a" )
+
+# Import target "odfaeg::odfaeg-math" for configuration "Debug"
+set_property(TARGET odfaeg::odfaeg-math APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(odfaeg::odfaeg-math PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libodfaeg-math.a"
+  )
+
+list(APPEND _cmake_import_check_targets odfaeg::odfaeg-math )
+list(APPEND _cmake_import_check_files_for_odfaeg::odfaeg-math "${_IMPORT_PREFIX}/lib/libodfaeg-math.a" )
+
+# Import target "odfaeg::odfaeg-math-mod" for configuration "Debug"
+set_property(TARGET odfaeg::odfaeg-math-mod APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(odfaeg::odfaeg-math-mod PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libodfaeg-math-mod.a"
+  )
+
+list(APPEND _cmake_import_check_targets odfaeg::odfaeg-math-mod )
+list(APPEND _cmake_import_check_files_for_odfaeg::odfaeg-math-mod "${_IMPORT_PREFIX}/lib/libodfaeg-math-mod.a" )
+
+# Import target "odfaeg::odfaeg-entity" for configuration "Debug"
+set_property(TARGET odfaeg::odfaeg-entity APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(odfaeg::odfaeg-entity PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libodfaeg-entity.a"
+  )
+
+list(APPEND _cmake_import_check_targets odfaeg::odfaeg-entity )
+list(APPEND _cmake_import_check_files_for_odfaeg::odfaeg-entity "${_IMPORT_PREFIX}/lib/libodfaeg-entity.a" )
+
+# Import target "odfaeg::odfaeg-entity-mod" for configuration "Debug"
+set_property(TARGET odfaeg::odfaeg-entity-mod APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(odfaeg::odfaeg-entity-mod PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libodfaeg-entity-mod.a"
+  )
+
+list(APPEND _cmake_import_check_targets odfaeg::odfaeg-entity-mod )
+list(APPEND _cmake_import_check_files_for_odfaeg::odfaeg-entity-mod "${_IMPORT_PREFIX}/lib/libodfaeg-entity-mod.a" )
+
+# Import target "odfaeg::odfaeg-physics" for configuration "Debug"
+set_property(TARGET odfaeg::odfaeg-physics APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(odfaeg::odfaeg-physics PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libodfaeg-physics.a"
+  )
+
+list(APPEND _cmake_import_check_targets odfaeg::odfaeg-physics )
+list(APPEND _cmake_import_check_files_for_odfaeg::odfaeg-physics "${_IMPORT_PREFIX}/lib/libodfaeg-physics.a" )
+
+# Import target "odfaeg::odfaeg-physics-mod" for configuration "Debug"
+set_property(TARGET odfaeg::odfaeg-physics-mod APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(odfaeg::odfaeg-physics-mod PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libodfaeg-physics-mod.a"
+  )
+
+list(APPEND _cmake_import_check_targets odfaeg::odfaeg-physics-mod )
+list(APPEND _cmake_import_check_files_for_odfaeg::odfaeg-physics-mod "${_IMPORT_PREFIX}/lib/libodfaeg-physics-mod.a" )
+
+# Import target "odfaeg::odfaeg-window" for configuration "Debug"
+set_property(TARGET odfaeg::odfaeg-window APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(odfaeg::odfaeg-window PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libodfaeg-window.a"
+  )
+
+list(APPEND _cmake_import_check_targets odfaeg::odfaeg-window )
+list(APPEND _cmake_import_check_files_for_odfaeg::odfaeg-window "${_IMPORT_PREFIX}/lib/libodfaeg-window.a" )
+
+# Import target "odfaeg::odfaeg-window-mod" for configuration "Debug"
+set_property(TARGET odfaeg::odfaeg-window-mod APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(odfaeg::odfaeg-window-mod PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libodfaeg-window-mod.a"
+  )
+
+list(APPEND _cmake_import_check_targets odfaeg::odfaeg-window-mod )
+list(APPEND _cmake_import_check_files_for_odfaeg::odfaeg-window-mod "${_IMPORT_PREFIX}/lib/libodfaeg-window-mod.a" )
+
+# Import target "odfaeg::odfaeg-graphics" for configuration "Debug"
+set_property(TARGET odfaeg::odfaeg-graphics APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(odfaeg::odfaeg-graphics PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libodfaeg-graphics.a"
+  )
+
+list(APPEND _cmake_import_check_targets odfaeg::odfaeg-graphics )
+list(APPEND _cmake_import_check_files_for_odfaeg::odfaeg-graphics "${_IMPORT_PREFIX}/lib/libodfaeg-graphics.a" )
+
+# Import target "odfaeg::odfaeg-graphics-mod" for configuration "Debug"
+set_property(TARGET odfaeg::odfaeg-graphics-mod APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(odfaeg::odfaeg-graphics-mod PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libodfaeg-graphics-mod.a"
+  )
+
+list(APPEND _cmake_import_check_targets odfaeg::odfaeg-graphics-mod )
+list(APPEND _cmake_import_check_files_for_odfaeg::odfaeg-graphics-mod "${_IMPORT_PREFIX}/lib/libodfaeg-graphics-mod.a" )
+
+# Import target "odfaeg::vma" for configuration "Debug"
+set_property(TARGET odfaeg::vma APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(odfaeg::vma PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libvma.a"
+  )
+
+list(APPEND _cmake_import_check_targets odfaeg::vma )
+list(APPEND _cmake_import_check_files_for_odfaeg::vma "${_IMPORT_PREFIX}/lib/libvma.a" )
+
+# Import target "odfaeg::meshoptimizer" for configuration "Debug"
+set_property(TARGET odfaeg::meshoptimizer APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(odfaeg::meshoptimizer PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libmeshoptimizer.a"
+  )
+
+list(APPEND _cmake_import_check_targets odfaeg::meshoptimizer )
+list(APPEND _cmake_import_check_files_for_odfaeg::meshoptimizer "${_IMPORT_PREFIX}/lib/libmeshoptimizer.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
