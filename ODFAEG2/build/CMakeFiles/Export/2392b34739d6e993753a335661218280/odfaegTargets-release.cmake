a04b97584d81f0e7e38c7ab9975a17f649c6e2d0
Laurent <laurentduroisin@gmail.com>
2026-07-01 15:34:11 +0200

    solving deathlock issue

--- ODFAEG2/build/CMakeFiles/Export/2392b34739d6e993753a335661218280/odfaegTargets-release.cmake
+++ ODFAEG2/build/CMakeFiles/Export/2392b34739d6e993753a335661218280/odfaegTargets-release.cmake
@@ -1,11 +1,11 @@
 #----------------------------------------------------------------
-# Generated CMake target import file for configuration "RELEASE".
+# Generated CMake target import file for configuration "Release".
 #----------------------------------------------------------------
 
 # Commands may need to know the format version.
 set(CMAKE_IMPORT_FILE_VERSION 1)
 
-# Import target "odfaeg::odfaeg-core" for configuration "RELEASE"
+# Import target "odfaeg::odfaeg-core" for configuration "Release"
 set_property(TARGET odfaeg::odfaeg-core APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(odfaeg::odfaeg-core PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libodfaeg-core.a"
@@ -14,7 +14,7 @@ set_target_properties(odfaeg::odfaeg-core PROPERTIES
 list(APPEND _cmake_import_check_targets odfaeg::odfaeg-core )
 list(APPEND _cmake_import_check_files_for_odfaeg::odfaeg-core "${_IMPORT_PREFIX}/lib/libodfaeg-core.a" )
 
-# Import target "odfaeg::odfaeg-core-mod" for configuration "RELEASE"
+# Import target "odfaeg::odfaeg-core-mod" for configuration "Release"
 set_property(TARGET odfaeg::odfaeg-core-mod APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(odfaeg::odfaeg-core-mod PROPERTIES
   IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
@@ -24,7 +24,7 @@ set_target_properties(odfaeg::odfaeg-core-mod PROPERTIES
 list(APPEND _cmake_import_check_targets odfaeg::odfaeg-core-mod )
 list(APPEND _cmake_import_check_files_for_odfaeg::odfaeg-core-mod "${_IMPORT_PREFIX}/lib/libodfaeg-core-mod.a" )
 
-# Import target "odfaeg::odfaeg-math" for configuration "RELEASE"
+# Import target "odfaeg::odfaeg-math" for configuration "Release"
 set_property(TARGET odfaeg::odfaeg-math APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(odfaeg::odfaeg-math PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libodfaeg-math.a"
@@ -33,7 +33,7 @@ set_target_properties(odfaeg::odfaeg-math PROPERTIES
 list(APPEND _cmake_import_check_targets odfaeg::odfaeg-math )
 list(APPEND _cmake_import_check_files_for_odfaeg::odfaeg-math "${_IMPORT_PREFIX}/lib/libodfaeg-math.a" )
 
-# Import target "odfaeg::odfaeg-math-mod" for configuration "RELEASE"
+# Import target "odfaeg::odfaeg-math-mod" for configuration "Release"
 set_property(TARGET odfaeg::odfaeg-math-mod APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(odfaeg::odfaeg-math-mod PROPERTIES
   IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
@@ -43,7 +43,7 @@ set_target_properties(odfaeg::odfaeg-math-mod PROPERTIES
 list(APPEND _cmake_import_check_targets odfaeg::odfaeg-math-mod )
 list(APPEND _cmake_import_check_files_for_odfaeg::odfaeg-math-mod "${_IMPORT_PREFIX}/lib/libodfaeg-math-mod.a" )
 
-# Import target "odfaeg::odfaeg-entity" for configuration "RELEASE"
+# Import target "odfaeg::odfaeg-entity" for configuration "Release"
 set_property(TARGET odfaeg::odfaeg-entity APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(odfaeg::odfaeg-entity PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libodfaeg-entity.a"
@@ -52,7 +52,7 @@ set_target_properties(odfaeg::odfaeg-entity PROPERTIES
 list(APPEND _cmake_import_check_targets odfaeg::odfaeg-entity )
 list(APPEND _cmake_import_check_files_for_odfaeg::odfaeg-entity "${_IMPORT_PREFIX}/lib/libodfaeg-entity.a" )
 
-# Import target "odfaeg::odfaeg-entity-mod" for configuration "RELEASE"
+# Import target "odfaeg::odfaeg-entity-mod" for configuration "Release"
 set_property(TARGET odfaeg::odfaeg-entity-mod APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(odfaeg::odfaeg-entity-mod PROPERTIES
   IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
@@ -62,7 +62,7 @@ set_target_properties(odfaeg::odfaeg-entity-mod PROPERTIES
 list(APPEND _cmake_import_check_targets odfaeg::odfaeg-entity-mod )
 list(APPEND _cmake_import_check_files_for_odfaeg::odfaeg-entity-mod "${_IMPORT_PREFIX}/lib/libodfaeg-entity-mod.a" )
 
-# Import target "odfaeg::odfaeg-physics" for configuration "RELEASE"
+# Import target "odfaeg::odfaeg-physics" for configuration "Release"
 set_property(TARGET odfaeg::odfaeg-physics APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(odfaeg::odfaeg-physics PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libodfaeg-physics.a"
@@ -71,7 +71,7 @@ set_target_properties(odfaeg::odfaeg-physics PROPERTIES
 list(APPEND _cmake_import_check_targets odfaeg::odfaeg-physics )
 list(APPEND _cmake_import_check_files_for_odfaeg::odfaeg-physics "${_IMPORT_PREFIX}/lib/libodfaeg-physics.a" )
 
-# Import target "odfaeg::odfaeg-physics-mod" for configuration "RELEASE"
+# Import target "odfaeg::odfaeg-physics-mod" for configuration "Release"
 set_property(TARGET odfaeg::odfaeg-physics-mod APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(odfaeg::odfaeg-physics-mod PROPERTIES
   IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
@@ -81,7 +81,7 @@ set_target_properties(odfaeg::odfaeg-physics-mod PROPERTIES
 list(APPEND _cmake_import_check_targets odfaeg::odfaeg-physics-mod )
 list(APPEND _cmake_import_check_files_for_odfaeg::odfaeg-physics-mod "${_IMPORT_PREFIX}/lib/libodfaeg-physics-mod.a" )
 
-# Import target "odfaeg::odfaeg-window" for configuration "RELEASE"
+# Import target "odfaeg::odfaeg-window" for configuration "Release"
 set_property(TARGET odfaeg::odfaeg-window APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(odfaeg::odfaeg-window PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libodfaeg-window.a"
@@ -90,7 +90,7 @@ set_target_properties(odfaeg::odfaeg-window PROPERTIES
 list(APPEND _cmake_import_check_targets odfaeg::odfaeg-window )
 list(APPEND _cmake_import_check_files_for_odfaeg::odfaeg-window "${_IMPORT_PREFIX}/lib/libodfaeg-window.a" )
 
-# Import target "odfaeg::odfaeg-window-mod" for configuration "RELEASE"
+# Import target "odfaeg::odfaeg-window-mod" for configuration "Release"
 set_property(TARGET odfaeg::odfaeg-window-mod APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(odfaeg::odfaeg-window-mod PROPERTIES
   IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
@@ -100,7 +100,7 @@ set_target_properties(odfaeg::odfaeg-window-mod PROPERTIES
 list(APPEND _cmake_import_check_targets odfaeg::odfaeg-window-mod )
 list(APPEND _cmake_import_check_files_for_odfaeg::odfaeg-window-mod "${_IMPORT_PREFIX}/lib/libodfaeg-window-mod.a" )
 
-# Import target "odfaeg::odfaeg-graphics" for configuration "RELEASE"
+# Import target "odfaeg::odfaeg-graphics" for configuration "Release"
 set_property(TARGET odfaeg::odfaeg-graphics APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(odfaeg::odfaeg-graphics PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libodfaeg-graphics.a"
@@ -109,7 +109,7 @@ set_target_properties(odfaeg::odfaeg-graphics PROPERTIES
 list(APPEND _cmake_import_check_targets odfaeg::odfaeg-graphics )
 list(APPEND _cmake_import_check_files_for_odfaeg::odfaeg-graphics "${_IMPORT_PREFIX}/lib/libodfaeg-graphics.a" )
 
-# Import target "odfaeg::odfaeg-graphics-mod" for configuration "RELEASE"
+# Import target "odfaeg::odfaeg-graphics-mod" for configuration "Release"
 set_property(TARGET odfaeg::odfaeg-graphics-mod APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(odfaeg::odfaeg-graphics-mod PROPERTIES
   IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
@@ -119,7 +119,7 @@ set_target_properties(odfaeg::odfaeg-graphics-mod PROPERTIES
 list(APPEND _cmake_import_check_targets odfaeg::odfaeg-graphics-mod )
 list(APPEND _cmake_import_check_files_for_odfaeg::odfaeg-graphics-mod "${_IMPORT_PREFIX}/lib/libodfaeg-graphics-mod.a" )
 
-# Import target "odfaeg::vma" for configuration "RELEASE"
+# Import target "odfaeg::vma" for configuration "Release"
 set_property(TARGET odfaeg::vma APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(odfaeg::vma PROPERTIES
   IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
@@ -129,7 +129,7 @@ set_target_properties(odfaeg::vma PROPERTIES
 list(APPEND _cmake_import_check_targets odfaeg::vma )
 list(APPEND _cmake_import_check_files_for_odfaeg::vma "${_IMPORT_PREFIX}/lib/libvma.a" )
 
-# Import target "odfaeg::meshoptimizer" for configuration "RELEASE"
+# Import target "odfaeg::meshoptimizer" for configuration "Release"
 set_property(TARGET odfaeg::meshoptimizer APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(odfaeg::meshoptimizer PROPERTIES
   IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
