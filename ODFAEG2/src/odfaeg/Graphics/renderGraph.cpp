5c5617d31cfd8318785b5433271e380eb232ca93
Laurent Duroisin <laurentduroisin@gmail7601.com>
2026-07-19 19:09:46 +0200

    render graph (following)

--- ODFAEG2/src/odfaeg/Graphics/renderGraph.cpp
+++ ODFAEG2/src/odfaeg/Graphics/renderGraph.cpp
@@ -1,16 +1,47 @@
 module;
 #include <string>
+#include <vulkan/vulkan.hpp>
 module odfaeg.graphic.renderGraph;
+import odfaeg.graphic.linkedListRenderer;
+import odfaeg.graphic.shadowRenderer;
 namespace odfaeg {
     namespace graphic {
         void RenderGraph::addLinkedListPass(RenderTarget& output, unsigned int layer, std::string typesToRender, unsigned int windowId) {
-
+            LinkedListRenderer* llr = new LinkedListRenderer(output, layer, typesToRender, windowId);
+            renderers.insert(std::make_pair(layer, llr));
         }
         void RenderGraph::addShadowPass(RenderTarget& output, RenderTexture& input,  unsigned int layer, std::string typesToRender, unsigned int windowId) {
-
+            llSMTransitionPoint = layer;
+            ShadowRenderer* sr = new ShadowRenderer(output, input, layer, typesToRender, windowId);
+            renderers.insert(std::make_pair(layer, sr));
+            inputShadowRT = &input;
+        }
+        std::vector<IComponent*> RenderGraph::getComponents() {
+            std::vector<IComponent*> components;
+            std::map<unsigned int, IRenderer*>::iterator it;
+            for (it = renderers.begin(); it != renderers.end(); it++) {
+                components.push_back(it->second);
+            }
+            std::map<unsigned int, Widget*>::iterator it2;
+            for (it2 = widgets.begin(); it2 != widgets.end(); it2++) {
+                components.push_back(it2->second);
+            }
+            return components;
         }
         void RenderGraph::render() {
-
+            std::map<unsigned int, IRenderer*>::iterator it;
+            for (it = renderers.begin(); it != renderers.end(); it++) {
+                it->second->clear();
+                if (it->first == llSMTransitionPoint) {
+                    inputShadowRT->beginRecordCommandBuffer();
+                    Texture::transitionImageLayout(inputShadowRT->getTexture().getImage(inputShadowRT->getImageIndex()), inputShadowRT->getCommandPool().getHandle(inputShadowRT->getCurrentFrame()), VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL, VK_IMAGE_LAYOUT_READ_ONLY_OPTIMAL);
+                    inputShadowRT->submit(true);
+                }
+                it->second->draw();
+            }
+            inputShadowRT->beginRecordCommandBuffer();
+            Texture::transitionImageLayout(inputShadowRT->getTexture().getImage(inputShadowRT->getImageIndex()), inputShadowRT->getCommandPool().getHandle(inputShadowRT->getCurrentFrame()), VK_IMAGE_LAYOUT_READ_ONLY_OPTIMAL, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL);   
+            inputShadowRT->submit(true);         
         }
     }
 }
--- ODFAEG2/src/odfaeg/Graphics/renderGraph.ixx
+++ ODFAEG2/src/odfaeg/Graphics/renderGraph.ixx
@@ -2,11 +2,13 @@ module;
 #include <map>
 #include <string>
 export module odfaeg.graphic.renderGraph;
+import odfaeg.graphic.iComponent;
 import odfaeg.graphic.iRenderer;
 import odfaeg.graphic.widget;
 import odfaeg.math.vec;
 import odfaeg.graphic.renderTarget;
 import odfaeg.graphic.renderTexture;
+import odfaeg.graphic.texture;
 namespace odfaeg {
     namespace graphic {
         export class RenderGraph {
@@ -14,9 +16,12 @@ namespace odfaeg {
             void addLinkedListPass(RenderTarget& output, unsigned int layer, std::string typesToRender, unsigned int windowId=-1);
             void addShadowPass(RenderTarget& output, RenderTexture& input,  unsigned int layer, std::string typesToRender, unsigned int windowId=-1);
             void render();
+            std::vector<IComponent*> getComponents();            
             private :
+            unsigned int llSMTransitionPoint;
             std::map<unsigned int, IRenderer*> renderers;
             std::map<unsigned int, Widget*> widgets;
+            RenderTexture* inputShadowRT;
         };
     }
 }
