c7aec8aecc438450d47dc77dd3d5375bd7f0c44f
Laurent Duroisin <laurentduroisin@gmail7601.com>
2026-08-03 10:32:32 +0200

    #include backend because of clang crash with modules

--- ODFAEG2/src/odfaeg/Window/action.cxx
+++ ODFAEG2/src/odfaeg/Window/action.cxx
@@ -1,3 +1,9 @@
+module;
+#include <map>
+#include <functional>
+#include <memory>
+#include <vector>
+#include <iostream>
 module odfaeg.window.action;
 import odfaeg.window.command;
 #include "action.inl"
