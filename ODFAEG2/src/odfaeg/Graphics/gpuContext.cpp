315bf4fc6dee7486e2eee9cc48472282e6b8bb17
Laurent Duroisin <laurentduroisin@gmail7601.com>
2026-08-03 18:42:55 +0200

    #include backend because of clang crash with modules

--- ODFAEG2/src/odfaeg/Graphics/gpuContext.cpp
+++ ODFAEG2/src/odfaeg/Graphics/gpuContext.cpp
@@ -1,4 +1,2 @@
 #include "gpuContext.hpp"
-#include "vertexBuffer.hpp"
-#include "texture.hpp"
 #include "gpuContext.inl"
--- ODFAEG2/src/odfaeg/Graphics/gpuContext.hpp
+++ ODFAEG2/src/odfaeg/Graphics/gpuContext.hpp
@@ -56,4 +56,5 @@ namespace odfaeg {
         };
     }
 }
+//#include "gpuContext.inl"
 #endif
